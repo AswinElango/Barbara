4d6e39a68fa6929c7732c08b523fb7b202494cf2
Aravindan22 <aravindan7822@gmail.com>
2020-12-31 13:27:22 +0530

    fourth line added

--- gitguit.py
+++ gitguit.py
@@ -18,27 +18,25 @@ T=Text(root,height = 5, width = 52)
 l.pack()
 T.pack()
 
-def git_commit():
+def git_commit(*push_call):
     msg=T.get("1.0",'end-1c')
     if(len(msg)>5):
-        git_add()
-        
+        os.system("git add .")
         cmd="git commit -m "+'"'+msg+'"'
         print(cmd)
         os.system(cmd)
-        
-        messagebox.showinfo("Commited")
-        
+        if(not(push_call)):
+            messagebox.showinfo("Commited")
     else:
         messagebox.showerror("enter commit message")
         pass
 commit_btn = Button(root,text="git commit",command = git_commit)
 commit_btn.pack()
 def git_push():
+    git_commit(True)
     cmd="git push"
     os.system(cmd)
     messagebox.showinfo("Pushed")
-   
    
 push_btn = Button(root,text="git push",command = git_push)
 push_btn.pack()
